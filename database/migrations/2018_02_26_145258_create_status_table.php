8e27c299e55fc059454d65ffdf5ff6002b5ad687
Joe Palala <jose.palala@live.com>
2018-02-26 23:11:31 +0800

    added authentication initial files and created migrations

---
+++ database/migrations/2018_02_26_145258_create_status_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStatusTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('status', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('task_id')->nullable(false);
+            $table->enum('status', ['CANCELLED','DOING','INCOMPLETE','COMPLETE', 'DELEGATED']);
+            $table->dateTime('updated_at');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('status');
+    }
+}
